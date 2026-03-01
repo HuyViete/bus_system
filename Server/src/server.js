ba76e0b8a56af098feedb65c8f11620c9db28e2f
HuyViete <huy.voviete@hcmut.edu.vn>
2026-03-01 17:15:27 +0700

    gps from osm

--- Server/src/server.js
+++ Server/src/server.js
@@ -1,11 +1,15 @@
 import express from 'express'
+import dotenv from 'dotenv'
+
+dotenv.config()
 
 const app = express()
 
+
 app.use(express.json())
 
-app.listen(3000, () => {
-    console.log('Server is running on port 3000')
+app.listen(process.env.PORT, () => {
+    console.log('Server is running on port ', process.env.PORT)
 })
 
 app.get('/', (req, res) => {
